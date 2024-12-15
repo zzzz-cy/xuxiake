e1996061e9ca5e93956614791b2284c4af446089
cydsimple <1418759712@qq.com>
2024-12-16 04:57:04 +0800

    12/16

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/xuxiake/",
+  publicPath: "./",
   outputDir: "dist",
   assetsDir:"static",
 })
